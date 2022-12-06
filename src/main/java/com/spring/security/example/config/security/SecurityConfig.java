ada000544a00c67cba09bb884f0e329742efd875
naskavinda <naskavinda@gmail.com>
2022-12-06 22:01:45 +0530

    login using InMemory User Details

---
+++ src/main/java/com/spring/security/example/config/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.spring.security.example.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                .requestMatchers("/api").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin(withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("naskavinda")
+                        .password("{noop}password")
+                        .authorities("ROLE_USER")
+                        .build()
+        );
+    }
+}
